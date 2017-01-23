cd532049f4424cf3f1fe57ea749fa330bf748dca
Cristian Berrios <cristian.berrios@headspring.com>
2017-01-23 00:35:52 -0600

    Add editorconfig. Add yarn lock. Add component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/searchBar'
 
 // get one from developer console
 const API_KEY = 'AIzaSyAD9BVK_K9AvnLwpTdXbHpKLYA6oU8pdh8'
 
 // Create new component
-const App = () => <div>Hey y'all</div>
+const App = () => <div>
+  <SearchBar />
+</div>
 
 // Render component to DOM
 ReactDOM.render(<App />, document.getElementById('app'))
