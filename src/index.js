915c49821376cbbb7aa921aba2e05e04cb84bd13
Cristian Berrios <cristian.berrios@headspring.com>
2017-01-22 22:39:25 -0600

    Add component files. Add api key. Add dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// get one from developer console
+const API_KEY = 'AIzaSyAD9BVK_K9AvnLwpTdXbHpKLYA6oU8pdh8'
+
 // Create new component
 const App = () => <div>Hey y'all</div>
 
